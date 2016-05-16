be356bd35011726adf2873622299fdbcc6aebd64
Brad Dennis <bcdennis@gmail.com>
2016-05-16 07:36:41 -0500

    adding in Deep Copy to the review package

--- src/review/DeepCopy.java
+++ src/review/DeepCopy.java
@@ -12,7 +12,11 @@ import java.util.List;
  * Exercises for the Deep Copy Tutorial
  *
  * 1.) Read: http://msoe.us/taylor/tutorial/cs2852/clone
- * 2.) Do exercises marked with TODO
+ * 2.) Review the code: https://github.com/bcdennis/cs2852/blob/master/src/review/DeepCopy.java
+ * 3.) Answer questions at : http://goo.gl/forms/LXLn8fkdhs
+ * 4.) Do exercises
+ * 5.) Uncomment out the prints
+ * 6.) Answer again: http://goo.gl/forms/LXLn8fkdhs
  *
  * @author Brad Dennis, Ph.D.
  * @version 1.0
